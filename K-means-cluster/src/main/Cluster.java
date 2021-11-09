6649a7c4adba983672eb4f8c0d5aa0701075860d
AidanDunleavy <aidandunleavy00@gmail.com>
2021-11-09 11:32:20 -0600

    Set up for the program.

---
+++ K-means-cluster/src/main/Cluster.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Cluster {
+	
+	protected ArrayList<Double[]> points;
+
+	public Cluster(ArrayList<Double[]> points) {
+		// TODO Auto-generated constructor stub
+		this.points = points;
+	}
+
+}
